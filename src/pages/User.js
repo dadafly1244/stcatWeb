259cdc83dee73e05da09ea10aa2a6b3649c209ac
YANGDAYOUNG <jc1525@hanmail.net>
2021-05-26 21:02:50 +0900

    에러수정

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Account } from '../components/Accounts';
-import Attributes from "../components/Attributes";
+
 //I love you dayoung
 const  User = ({match}) => {
     return(
         <div>
-            <h2>Hello! {match.params.name}</h2>
-            <Account>
-                <Attributes />
-            </Account>
+            <h2>Hello! {}</h2>
             
         </div>
     )
