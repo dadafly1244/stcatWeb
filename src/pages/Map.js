e794a268abcb74f5d79a4728626c8ccdb19a74fa
YANGDAYOUNG <jc1525@hanmail.net>
2021-05-28 14:57:50 +0900

    map 되랏!!!!

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -73,12 +73,12 @@ const Map = () => {
     }
     var container = document.getElementById('map');
     var options = {
-      center: new kakao.maps.LatLng(locationData),
+      center: new kakao.maps.LatLng(latData,lonData),
       level: 3
     };
     
     var map = new kakao.maps.Map(container, options);
-    var markerPosition  = new kakao.maps.LatLng(locationData); 
+    var markerPosition  = new kakao.maps.LatLng(latData,lonData); 
     var marker = new kakao.maps.Marker({
       position: markerPosition
     });
